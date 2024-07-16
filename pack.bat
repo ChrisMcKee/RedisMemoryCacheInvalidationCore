05e6a3ae85b6acddd9529a8efc171bd38a99d678
Chris McKee <chris@chrismckee.co.uk>
2024-07-16 11:36:05 +0100

    v2.4.0

--- pack.bat
+++ pack.bat
@@ -1,6 +1,6 @@
 echo off
 SET VERSION=%1
-IF [%VERSION%]==[] SET VERSION=2.3.0-local
+IF [%VERSION%]==[] SET VERSION=2.4.0-local
 
 dotnet build -c Release RedisMemoryCacheInvalidation.sln
 dotnet pack src\RedisMemoryCacheInvalidationCore\RedisMemoryCacheInvalidationCore.csproj -c Release --include-symbols --nologo -o artifacts/ -p:PackageVersion=%VERSION%
